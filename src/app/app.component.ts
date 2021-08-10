02c1b55bf6ed84f314d2941b51f4cd9fb4a08c31
Yoeri Moens <yoerimoens@gmail.com>
2021-08-10 22:36:15 +0200

    Connect to apollo-server-subscriptions and fetch all users using GraphQL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Observable } from "rxjs"
+import { Query, User } from "./types"
+import { Apollo, gql } from "apollo-angular"
+import { map } from "rxjs/operators";
+
+const GET_USERS = gql`
+  query users {
+    users {
+      id
+      name
+      email
+    }
+  }
+`;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +20,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'apollo-client-subscriptions';
+  title: string = 'TITLE'
+  constructor(private apollo: Apollo) {}
+  users: Observable<User[]> = this.apollo
+  .watchQuery<Query>({
+    query: GET_USERS
+  })
+  .valueChanges.pipe(map(result => result.data.users));
 }
---
+++ src/app/types.ts
@@ -0,0 +1,30 @@
+// export type Author = {
+//   id: number;
+//   firstName: string;
+//   lastName: string;
+// }
+
+export type User = {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type Query = {
+  users: User[];
+};
+
+// export type Post = {
+//   id: number;
+//   title: string;
+//   votes: number;
+// }
+
+// export type Query = {
+//   posts: Post[];
+// }
+
+// export type Mutation = {
+//   upvotePost: Post;
+// }
